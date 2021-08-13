11ab59ede20420203c564118c27b01a581a94058
orionalfa <orionalfacentauri@gmail.com>
2021-08-13 12:18:07 +0200

    connection with db

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,17 +1,32 @@
 const app = require("./server");
 const { config } = require("./config");
+const db = require("./models/product-model");
 
-// const connect = require("./db/connect");
+const connect = require("./db/connect");
 
 // const { seedMovies, seedMovieGenres } = require("./db/seed");
 
-// connect().then(async function onServerInit() {
-//   config.logger.info(`DB connected`);
+connect().then(async function onServerInit() {
+  console.log(`DB connected`);
 
-// await seedMovieGenres();
-// await seedMovies();
+  //   const { _id } = await db.Models.create({
+  //     name: "Flying Microtonal Banana",
+  //     price: 2000,
+  //     modelDescription: "this banana will make you fly",
+  //     unitsStock: 10,
+  //     images: ["imageUrl"],
+  //   });
 
-app.listen(config.app.PORT, () => {
-  console.log(`Server running at http://localhost:${config.app.PORT}`);
+  //   await db.Products.create({
+  //     title: "Microtonal Banana",
+  //     description: "The Banana from KGLW",
+  //     models: [_id],
+  //   });
+
+  // await seedMovieGenres();
+  // await seedMovies();
+
+  app.listen(config.app.PORT, () => {
+    console.log(`Server running at http://localhost:${config.app.PORT}`);
+  });
 });
-// });
