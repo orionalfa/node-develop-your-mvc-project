18092bd1570dcca86d749807658c4c597a85ec95
orionalfa <orionalfacentauri@gmail.com>
2021-08-13 10:20:23 +0200

    server init

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,1 +1,17 @@
-// test;
+const app = require("./server");
+const { config } = require("./config");
+
+// const connect = require("./db/connect");
+
+// const { seedMovies, seedMovieGenres } = require("./db/seed");
+
+// connect().then(async function onServerInit() {
+//   config.logger.info(`DB connected`);
+
+// await seedMovieGenres();
+// await seedMovies();
+
+app.listen(config.app.PORT, () => {
+  console.log(`Server running at http://localhost:${config.app.PORT}`);
+});
+// });
