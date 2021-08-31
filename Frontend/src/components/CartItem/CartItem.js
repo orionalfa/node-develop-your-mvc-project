6a8620b64b4e8889856dc8260e849a37daae33ca
Marc <marcsr03@gmail.com>
2021-08-31 12:40:22 +0200

    Cart WIP

--- Frontend/src/components/CartItem/CartItem.js
+++ Frontend/src/components/CartItem/CartItem.js
@@ -4,85 +4,88 @@ import { Button } from "@material-ui/core";
 // import 'bootstrap/dist/css/bootstrap.css';
 
 function buildSelectOptions(unitsInStock) {
-    return Array.from({ length: unitsInStock }, (_value, index) => {
-        const currentIndex = index + 1;
-        return (
-        <option key={currentIndex} value={currentIndex}>
-            {currentIndex}
-        </option>
-        );
-    });
+  return Array.from({ length: unitsInStock }, (_value, index) => {
+    const currentIndex = index + 1;
+    return (
+      <option key={currentIndex} value={currentIndex}>
+        {currentIndex}
+      </option>
+    );
+  });
 }
 
-function CartItem({id,
-    img,
-    title,
-    price,
-    quantity,
-    unitsInStock,
-    handleChange,
-    handleRemove,
-    handleClose,
-  }) {
-    function onHandleChange(event) {
-      handleChange(event, id);
-    }
-    function onHandleRemove() {
-      handleRemove(id);
-    }
-  
-    return (
-    <div>
-      <li className="col">
-        <div className="row flex-column">
-          <div className="col">
-            <div className="row">
-              <div className="col-12 col-xl-4 mb-3 mb-xl-0">
-                <img className="ShoppingCartItem__img" src={img} alt="" />
-              </div>
-              <div className="col-12 col-xl-8">
-                <div className="row flex-column">
-                  <div className="col">
-                    <h4 className="h5">
-                      <strong>{title}</strong>
-                    </h4>
-                  </div>
-                  <div className="col">
-                    <p>
-                      <strong>{price}€</strong>
-                    </p>
-                  </div>
-                  <div className="col mt-auto">
-                    <div className="row">
-                      <div className="col col-6 col-lg-4">
-                        <select
-                          className="custom-select"
-                          onChange={onHandleChange}
-                          onBlur={onHandleChange}
-                          value={quantity}
-                        >
-                          {buildSelectOptions(unitsInStock)}
-                        </select>
-                      </div>
-                      <div className="col col-6 col-lg-8">
-                        <Button variant="contained" color="primary" onClick={onHandleRemove}>
-                            Remove
-                        </Button>
-                        {/* <Button onClick={onHandleRemove}>Remove</Button> */}
-                      </div>
+function CartItem({
+  id,
+  img,
+  title,
+  price,
+  quantity,
+  unitsInStock,
+  handleChange,
+  handleRemove,
+  // handleClose,
+}) {
+  function onHandleChange(event) {
+    handleChange(event, id);
+  }
+  function onHandleRemove() {
+    handleRemove(id);
+  }
+
+  return (
+    <li className="col">
+      <div className="row flex-column">
+        <div className="col">
+          <div className="row">
+            <div className="col-12 col-xl-4 mb-3 mb-xl-0">
+              <img className="cartItemImg" src={img} alt="" />
+            </div>
+            <div className="col-12 col-xl-8">
+              <div className="row flex-column">
+                <div className="col">
+                  <h4 className="h5">
+                    <strong>{title}</strong>
+                  </h4>
+                </div>
+                <div className="col">
+                  <p>
+                    <strong>{price}€</strong>
+                  </p>
+                </div>
+                <div className="col mt-auto">
+                  <div className="row">
+                    <div className="col col-6 col-lg-4">
+                      <select
+                        className="custom-select"
+                        onChange={onHandleChange}
+                        onBlur={onHandleChange}
+                        value={quantity}
+                      >
+                        {buildSelectOptions(unitsInStock)}
+                      </select>
+                    </div>
+                    <div className="col col-6 col-lg-8">
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={onHandleRemove}
+                      >
+                        Remove
+                      </Button>
+                      {/* <Button onClick={onHandleRemove}>Remove</Button> */}
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="col">
-            <hr />
-          </div>
         </div>
-      </li>
-    </div>
-    );
+        <div className="col">
+          <hr />
+        </div>
+      </div>
+    </li>
+  );
 }
 
 export default CartItem;
