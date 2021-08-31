39bd51fa3451b69a4e9b7bc8e9a2abd7de03bc0b
JonCroatan <43881219+jonCroatanUto@users.noreply.github.com>
2021-08-31 17:31:42 +0200

    home and product pages with Routes

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,16 +1,33 @@
 import "./App.css";
-
+import React from 'react';
+// import ReactDOM from 'react-dom';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import ProductPage from "./Pages/Products";
 import NavBar from "./components/NavBar";
+import Home from "./components/Home"
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header><NavBar/></header>
-      <main>
-        <ProductPage />
-      </main>
-    </div>
+    <>
+  <header><NavBar/></header>
+     
+  <BrowserRouter>
+    <Switch>
+      <Route 
+      path="/" 
+      exact 
+      component={Home}
+      />
+
+      <Route 
+      path="/products"
+      component={ProductPage}
+      />
+
+    </Switch>
+  </BrowserRouter>
+  </>
   );
 }
 
