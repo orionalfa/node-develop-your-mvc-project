fdf09d8fcdc399ba5d51cecf052cb18a1f5cefd6
JonCroatan <43881219+jonCroatanUto@users.noreply.github.com>
2021-09-01 14:49:43 +0200

    conecting front with database

--- Frontend/src/Pages/Products/ProductPage.js
+++ Frontend/src/Pages/Products/ProductPage.js
@@ -1,18 +1,43 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import $ from "jquery";
 import "./styles.css";
 
 import { Grid } from "@material-ui/core/";
 
 import ProductItem from "../../components/productItem";
 import Cart from "../../components/Cart";
+import { product } from "prelude-ls";
+ function getProducts(){
+  $.ajax({
+    url:"http://localhost:4000/products",
+    type:"GET",
+    success:(res)=>{
+      return res.data;
+    }
+  })
+}
 
 function ProductPage({ showShoppingCart }) {
+  const [products, setProducts]=useState([]);
+  const [isLoaded, setIsLoaded]=useState(false);
+  useEffect(()=>{
+   $.ajax({
+    url:"http://localhost:4000/products",
+    type:"GET",
+    success:(res)=>{
+      setProducts(res.data);
+      setIsLoaded(true);
+    }
+  })
+  },[])
+  console.log(products)
   return (
     <main>
+
       <Cart showShoppingCart={showShoppingCart} />
       <Grid container className="product-page-grid">
-        {Array.from(Array(12)).map((_, index) => (
+      {/* {isLoaded ? ( */}
+        {products.map((product, index) => (
           <Grid
             item
             p={10}
@@ -23,13 +48,15 @@ function ProductPage({ showShoppingCart }) {
             className="product-page-product-container"
           >
             <ProductItem
-              productTitle="Violin"
-              description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat"
+              productTitle={product.title}
+              description={product.description}
               price="300E"
               unitsInStock="units in stoK"
             ></ProductItem>
+          
           </Grid>
         ))}
+      {/* )} */}
       </Grid>
     </main>
   );
