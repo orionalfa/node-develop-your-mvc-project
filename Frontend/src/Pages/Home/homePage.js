3c99ac1ccb0aa2bd43673309acbff9917035869d
Marc <marcsr03@gmail.com>
2021-09-01 17:41:28 +0200

    fixed merge issues

--- Frontend/src/Pages/Home/homePage.js
+++ Frontend/src/Pages/Home/homePage.js
@@ -1,9 +1,9 @@
 import "./homeStyles.css";
-import logo from "../../img/logotype.png";
+// import logo from "./img/logotype.png";
 function Home(){
 return(
     <main className="first_image">
-    <img className="logotype" src={logo}></img>
+    <img className="logotype" src="./img/logotype.png"></img>
     
   </main>
 )
