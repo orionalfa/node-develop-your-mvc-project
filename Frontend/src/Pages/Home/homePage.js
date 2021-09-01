27b953e82e25f8ccf69b38558b7fbc5520eb6a92
JonCroatan <43881219+jonCroatanUto@users.noreply.github.com>
2021-09-01 11:42:38 +0200

    logo

--- Frontend/src/Pages/Home/homePage.js
+++ Frontend/src/Pages/Home/homePage.js
@@ -1,9 +1,10 @@
-import "./homeStyles.css"
-
+import "./homeStyles.css";
+import logo from "../../img/logotype.png";
 function Home(){
 return(
     <main className="first_image">
-    <img className="logotype" src="../../img/logotype.png"></img>
+    <img className="logotype" src={logo}></img>
+    
   </main>
 )
 }
