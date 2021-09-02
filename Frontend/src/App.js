47b77b587fd009c01a76cec3d670b6cffd33f7ab
Miguel Perez Martinez <migueldw2.0@gmail.com>
2021-09-02 12:50:42 +0200

    feat: router to all checkout pages and navbar list

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState, useEffect, useReducer} from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import $ from "jquery";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import musicContext from "./context";
@@ -12,6 +12,11 @@ import Home from "./Pages/Home";
 import ProductPage from "./Pages/Products";
 import SignUpPage from "./Pages/SignUp";
 import ChangePasswordPage from "./Pages/ChangePwd";
+import ShippingInfo from "./Pages/Checkout/ShippingInfo";
+import ShippingMethod from "./Pages/Checkout/ShippingMethod";
+import PaymentMethod from "./Pages/Checkout/PaymentMethod";
+import PreviewOrder from "./Pages/Checkout/PreviewOrder";
+import ConfirmOrder from "./Pages/Checkout/ConfirmOrder";
 // const SHOW_SHOPPINGCART="SHOW_SHOPPINGCART";
 // function reducer(state, action){
 //   switch (action.type){
@@ -21,68 +26,61 @@ import ChangePasswordPage from "./Pages/ChangePwd";
 //           showCart:showShoppingCart ? state=false : state=true
 //         }
 
-
 //     }
 //   }
 // }
 function App() {
   // const [state, dispatch] = useReducer(reducer);
   const [showShoppingCart, setShowShoppingCart] = useState(false);
-  const [products, setProducts]=useState([]);
-  const [isLoaded, setIsLoaded]=useState(false);
-  useEffect(()=>{
+  const [products, setProducts] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
     getAllProducts();
-  },[])
+  }, []);
 
   function showCart() {
     showShoppingCart ? setShowShoppingCart(false) : setShowShoppingCart(true);
   }
-  function getAllProducts(){
+  function getAllProducts() {
     $.ajax({
-      url:"http://localhost:4000/products",
-      type:"GET",
-      success:(res)=>{
+      url: "http://localhost:4000/products",
+      type: "GET",
+      success: (res) => {
         setProducts(res.data);
         setIsLoaded(true);
-      }
-    })
+      },
+    });
   }
   return (
-  <>
-    <header>
+    <>
+      <header>
         <NavBar showCart={showCart} />
-    </header>
-    <musicContext.Provider
-    value={{
-      showShoppingCart:showShoppingCart,
-      hola:"holaketal",
-      products:products,
-      isLoaded:isLoaded
-  }}
-    >
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route
-            path="/products"
-            render={() => (
-              <ProductPage />
-            )}
-          />
-         
-          <Route path="/user-pwd-change" component={ChangePasswordPage} />
-          <Route
-              path="/sign-up"
-              render={() => (
-                <SignUpPage  />
-              )}
-            />
-          <Route path="/product-dashboard" component={ProductDashboard} />
-        </Switch>
-      </BrowserRouter>
-    </musicContext.Provider>
-  </>
-      
+      </header>
+      <musicContext.Provider
+        value={{
+          showShoppingCart: showShoppingCart,
+          hola: "holaketal",
+          products: products,
+          isLoaded: isLoaded,
+        }}
+      >
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/products" render={() => <ProductPage />} />
+
+            <Route path="/user-pwd-change" component={ChangePasswordPage} />
+            <Route path="/sign-up" render={() => <SignUpPage />} />
+            <Route path="/product-dashboard" component={ProductDashboard} />
+            <Route path="/shipping-info" render={() => <ShippingInfo />} />
+            <Route path="/shipping-method" render={() => <ShippingMethod />} />
+            <Route path="/payment-method" render={() => <PaymentMethod />} />
+            <Route path="/preview-order" render={() => <PreviewOrder />} />
+            <Route path="/confirm-order" render={() => <ConfirmOrder />} />
+          </Switch>
+        </BrowserRouter>
+      </musicContext.Provider>
+    </>
   );
 }
 
