012b3d792decce5de4bc25956b2788846f195eb7
Miguel Perez Martinez <71381808+MiguelPerezMartinez@users.noreply.github.com>
2021-09-02 11:12:56 +0200

    Feat/login modal (#36)
    * feat: product grid implemnentation
    fixes #27
    
    * feat: basic grid structure, getAllProducts request
    fixes #28
    
    * cors
    
    * feat: login modal base
    
    * feat: login modal implemented
    fixes: #35

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,15 +2,19 @@ import "./App.css";
 import React, { useState } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
+import "./App.css";
+
 import NavBar from "./components/NavBar";
 
 import Home from "./Pages/Home";
 import ProductPage from "./Pages/Products";
 import SignUpPage from "./Pages/SignUp";
 import ChangePasswordPage from "./Pages/ChangePwd";
+import ProductDashboard from "./Pages/ProductDashboard";
 
 function App() {
   const [showShoppingCart, setShowShoppingCart] = useState(false);
+
   function showCart() {
     showShoppingCart ? setShowShoppingCart(false) : setShowShoppingCart(true);
   }
@@ -20,23 +24,33 @@ function App() {
       <header>
         <NavBar showCart={showCart} />
       </header>
-
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" exact component={Home} />
-
-          <Route
-            path="/products"
-            render={(props) => (
-              <ProductPage showShoppingCart={showShoppingCart} {...props} />
-            )}
-          />
-
-          <Route path="/sign-up" component={SignUpPage} />
-
-          <Route path="/user-pwd-change" component={ChangePasswordPage} />
-        </Switch>
-      </BrowserRouter>
+      <main>
+        <BrowserRouter>
+          <Switch>
+            <Route
+              path="/"
+              exact
+              render={(props) => (
+                <Home showShoppingCart={showShoppingCart} {...props} />
+              )}
+            />
+            <Route
+              path="/products"
+              render={(props) => (
+                <ProductPage showShoppingCart={showShoppingCart} {...props} />
+              )}
+            />
+            <Route
+              path="/sign-up"
+              render={(props) => (
+                <SignUpPage showShoppingCart={showShoppingCart} {...props} />
+              )}
+            />
+            <Route path="/user-pwd-change" component={ChangePasswordPage} />
+            <Route path="/product-dashboard" component={ProductDashboard} />
+          </Switch>
+        </BrowserRouter>
+      </main>
     </>
   );
 }
