a5b14cbe36c1a79ac445ed257dbd82459fefac13
JonCroatan <43881219+jonCroatanUto@users.noreply.github.com>
2021-09-03 11:43:11 +0200

    incuding login, Link and conditional render in Login page

--- Frontend/src/components/LogIn/LogIn.js
+++ Frontend/src/components/LogIn/LogIn.js
@@ -6,9 +6,27 @@ import "./styles.css";
 
 export default function LogIn(showLogin) {
   const [showLoginModal, setShowLoginModal] = useState(false);
+  const [userEmail, setUseremail] = useState();
+  const [userPassword, setPassword] = useState();
+  const [isLoggedin, setisLoggedin] = useState(true);
 
   function showLogin() {
     showLoginModal ? setShowLoginModal(false) : setShowLoginModal(true);
+  }
+
+  function LogInMethod() {
+    $.ajax({
+      url: "http://localhost:4000/users/sign-in",
+      type: "POST",
+      contentType: "application/json; charset=utf-8",
+      data: JSON.stringify({
+        email: userEmail,
+        pass: userPassword,
+      }),
+      success: (res) => {
+        console.log(res);
+      },
+    });
   }
 
   if (showLoginModal) {
@@ -17,29 +35,43 @@ export default function LogIn(showLogin) {
     $("#login-modal").css("display", "none");
   }
 
+  // const isLoggedin = true;
+
   return (
+    
     <div className="login-main-container">
-      <Link className="login-button" to="/sign-up">
-        Register
-      </Link>
+    {isLoggedin ? (
+      <>
+    <Link className="login-button" to="/sign-up">
+            Register
+          </Link>
       <div>
         <a id="login-button" className="login-button" onClick={showLogin}>
           LogIn
         </a>
         <div id="login-modal" className="login-container">
-          <div className="login-field">Username:</div>
+          <div className="login-field">Email:</div>
           <div className="login-field">
-            <input type="text" />
+            <input type="text" onChange={(e) => setUseremail(e.target.value)} />
           </div>
           <div className="login-field">Password:</div>
           <div className="login-field">
-            <input type="text" />
+            <input type="text" onChange={(e) => setPassword(e.target.value)} />
           </div>
           <div className="login-field login-buttons">
-            <a href="#">Login</a>
+            <button onClick={() => LogInMethod()}>Login</button>
+
           </div>
         </div>
       </div>
+      </>
+      
+      ) : (
+        <Link className="login-button" to="/logout">
+          Logout
+        </Link>
+       
+      )}
     </div>
   );
 }
