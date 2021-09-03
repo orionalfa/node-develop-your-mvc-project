19a57a353fe486b38aa8e07613f0ad7e801271cd
Marc <marcsr03@gmail.com>
2021-09-03 11:03:54 +0200

    again

--- Frontend/src/components/LogIn/LogIn.js
+++ Frontend/src/components/LogIn/LogIn.js
@@ -5,9 +5,26 @@ import "./styles.css";
 
 export default function LogIn(showLogin) {
   const [showLoginModal, setShowLoginModal] = useState(false);
+  const [userEmail, setUseremail] = useState();
+  const [userPassword, setPassword] = useState();
 
   function showLogin() {
     showLoginModal ? setShowLoginModal(false) : setShowLoginModal(true);
+  }
+
+  function LogInMethod() {
+    $.ajax({
+      url: "http://localhost:4000/users/sign-in",
+      type: "POST",
+      contentType: "application/json; charset=utf-8",
+      data: JSON.stringify({
+        email: userEmail,
+        pass: userPassword,
+      }),
+      success: (res) => {
+        console.log(res);
+      },
+    });
   }
 
   if (showLoginModal) {
@@ -30,14 +47,14 @@ export default function LogIn(showLogin) {
         <div id="login-modal" className="login-container">
           <div className="login-field">Username:</div>
           <div className="login-field">
-            <input type="text" />
+            <input type="text" onChange={(e) => setUseremail(e.target.value)} />
           </div>
           <div className="login-field">Password:</div>
           <div className="login-field">
-            <input type="text" />
+            <input type="text" onChange={(e) => setPassword(e.target.value)} />
           </div>
           <div className="login-field login-buttons">
-            <a href="#">Login</a>
+            <button onClick={() => LogInMethod()}>Login</button>
           </div>
         </div>
       </div>
