4fc8d8518f49361693a34230dcf43e6c686f1b12
Marc <marcsr03@gmail.com>
2021-09-03 15:37:27 +0200

    product update form wip

--- Frontend/src/Pages/ProductForm/ProductForm.js
+++ Frontend/src/Pages/ProductForm/ProductForm.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { Grid } from "@material-ui/core/";
+import Input from "../../components/Input";
 //import $ from "jquery";
 import "./styles.css";
 import musicContext from "../../context";
@@ -11,56 +12,159 @@ import musicContext from "../../context";
 // }
 
 export default function ProductForm() {
-  const { products, removeModel, updateProduct, getAllProducts } =
-    useContext(musicContext);
+  const product = {
+    title: "Eliminate",
+    description: "Producto a eliminar",
+    models: [
+      {
+        name: "Modelo 1 a elminar",
+        price: 2000,
+        modelDescription: "lalalala",
+        unitsStock: 10,
+        images: ["imageUrl"],
+      },
+      // {
+      //   name: "Modelo 2 a elminar",
+      //   price: 2000,
+      //   modelDescription: "lalalala",
+      //   unitsStock: 10,
+      //   images: ["imageUrl"],
+      // },
+    ],
+  };
 
-  if (!products) return null;
+  const [productTitle, setProductTitle] = useState(product.title);
+  const [productDescription, setProductDescription] = useState(product.description);
+  const { removeModel, updateProduct } = useContext(musicContext);
 
-  const productList = products.map((product, index) => (
-    <tr key={uuidv4()}>
-      <td key={uuidv4()}>{product.title}</td>
-      <td key={uuidv4()}>{product.description}</td>
-      <td key={uuidv4()}>{product.models[0].name}</td>
-      <td key={uuidv4()}>{product.models[0].price}</td>
-      <td key={uuidv4()}>{product.models[0].modelDescription}</td>
-      <td key={uuidv4()}>{product.models[0].unitsStock}</td>
-      <td key={uuidv4()}>{product.models[0].images}</td>
-      <td key={uuidv4()}>
-        <button onClick={() => updateProduct(product)}>Update</button>
-      </td>
-      <td key={uuidv4()}>
-        <button
-          onClick={() => {
-            removeModel(product);
-            getAllProducts();
-          }}
-        >
-          Remove
-        </button>
-      </td>
-    </tr>
-  ));
+  // const modelsList = product.models.map((product, index) => (
+
+  // ));
 
   return (
     <main>
-      <Grid container xs={12} sm={12} md={12}>
-        <div className="grid-container">
-          <table>
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Description</th>
-                <th>Name</th>
-                <th>Price</th>
-                <th>Model description</th>
-                <th>Units in stock</th>
-                <th>Images</th>
-              </tr>
-            </thead>
-            <tbody>{productList}</tbody>
-          </table>
+      <section className="container">
+        <div className="headerPage mt-3">
+          <h2>Create User Account</h2>
         </div>
-      </Grid>
+        <hr />
+        <div className="mb-3">
+          <form>
+            <div className="mb-3">
+              <Input
+                type="text"
+                label="Title:"
+                id="title"
+                defaultValue={product.title}
+                // value={formik.values.address}
+                placeholder="Introduce new title"
+                // handleChange={formik.handleChange}
+                // handleBlur={formik.handleBlur}
+                // hasErrorMessage={formik.touched.address}
+                // errorMessage={formik.errors.address}
+              />
+            </div>
+            <div className="mb-3">
+              <Input
+                type="text"
+                label="Description:"
+                id="description"
+                defaultValue={product.description}
+                // value={formik.values.address}
+                placeholder="Introduce new description"
+                // handleChange={formik.handleChange}
+                // handleBlur={formik.handleBlur}
+                // hasErrorMessage={formik.touched.address}
+                // errorMessage={formik.errors.address}
+              />
+            </div>
+            {product.models.map((model, index) => (
+              <div>
+                <div className="headerPage mt-3">
+                  <h5>Model {index + 1}</h5>
+                </div>
+                <div className="mb-3">
+                  <Input
+                    type="text"
+                    label="Name:"
+                    id="name"
+                    defaultValue={model.name}
+                    // value={formik.values.address}
+                    placeholder="Introduce new name"
+                    // handleChange={formik.handleChange}
+                    // handleBlur={formik.handleBlur}
+                    // hasErrorMessage={formik.touched.address}
+                    // errorMessage={formik.errors.address}
+                  />
+                </div>
+                <div className="mb-3">
+                  <Input
+                    type="text"
+                    label="Price:"
+                    id="price"
+                    defaultValue={model.price}
+                    // value={formik.values.address}
+                    placeholder="Introduce new price"
+                    // handleChange={formik.handleChange}
+                    // handleBlur={formik.handleBlur}
+                    // hasErrorMessage={formik.touched.address}
+                    // errorMessage={formik.errors.address}
+                  />
+                </div>
+                <div className="mb-3">
+                  <Input
+                    type="text"
+                    label="Description:"
+                    id="description"
+                    defaultValue={model.modelDescription}
+                    // value={formik.values.address}
+                    placeholder="Introduce new description"
+                    // handleChange={formik.handleChange}
+                    // handleBlur={formik.handleBlur}
+                    // hasErrorMessage={formik.touched.address}
+                    // errorMessage={formik.errors.address}
+                  />
+                </div>
+                <div className="mb-3">
+                  <Input
+                    type="text"
+                    label="Stock Units:"
+                    id="unitStock"
+                    defaultValue={model.unitsStock}
+                    // value={formik.values.address}
+                    placeholder="Update stock units"
+                    // handleChange={formik.handleChange}
+                    // handleBlur={formik.handleBlur}
+                    // hasErrorMessage={formik.touched.address}
+                    // errorMessage={formik.errors.address}
+                  />
+                </div>
+                {model.images.map((image) => (
+                  <div>
+                    <div className="mb-3">
+                      <Input
+                        type="text"
+                        label="Image:"
+                        id="image"
+                        defaultValue={image}
+                        // value={formik.values.address}
+                        placeholder="Update image"
+                        // handleChange={formik.handleChange}
+                        // handleBlur={formik.handleBlur}
+                        // hasErrorMessage={formik.touched.address}
+                        // errorMessage={formik.errors.address}
+                      />
+                    </div>
+                  </div>
+                ))}
+              </div>
+            ))}
+            <button type="submit" className="btn btn-primary">
+              Confirm
+            </button>
+          </form>
+        </div>
+      </section>
     </main>
   );
 }
