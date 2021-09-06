fcaaeb58d85b9fd1101a1fda0c22acc06ff06853
Miguel Perez Martinez <migueldw2.0@gmail.com>
2021-09-06 10:00:59 +0200

    feat: shopping cart

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -9,13 +9,29 @@ const connect = require("./db/connect");
 connect().then(async function onServerInit() {
   console.log(`DB connected`);
 
-  // const { _id } = await db.Models.create({
-  //   name: "Flying Microtonal Banana",
+  const { _id } = await db.Models.create({
+    name: "Flying Microtonal Banana",
+    price: 2000,
+    modelDescription: "this banana will make you fly",
+    unitsStock: 10,
+    images: [
+      "https://cdna.artstation.com/p/assets/video_clips/images/025/166/784/large/marius-k-thumb.jpg?1584876869",
+    ],
+  });
+
+  await db.Products.create({
+    title: "Microtonal Banana",
+    description: "The Banana from KGLW",
+    models: [_id],
+  });
+
+  // _id = await db.Models.create({
+  //   name: "Fender Stratocaster",
   //   price: 2000,
   //   modelDescription: "this banana will make you fly",
   //   unitsStock: 10,
   //   images: [
-  //     "https://cdna.artstation.com/p/assets/video_clips/images/025/166/784/large/marius-k-thumb.jpg?1584876869",
+  //     "https://thumbs.static-thomann.de/thumb//thumb580x/pics/cms/image/guide/es/online_guide_e_guitars/fender_stratocaster.jpg",
   //   ],
   // });
 
